a14c9a9f7bfb6ba2e2ef0ad28e158d28d89d8395
saralozano21 <slozanbr7@alumnes.ub.edu>
2025-11-25 12:07:47 +0000

    holonomic

--- src/my_robot_control/setup.py
+++ src/my_robot_control/setup.py
@@ -34,7 +34,7 @@ setup(
             'my_robot_control_exec_lidar = my_robot_control.my_robot_control_lidar:main',
             'my_robot_lidar_test_rUBot_exec = my_robot_control.my_robot_lidar_test_rUBot:main',
             'my_robot_selfcontrol_holonomic_exec = my_robot_control.my_robot_selfcontrol_holonomic:main',
-            'my_robot_wallfollower_hollonomic_exec = my_robot_control.my_robot_wallfollower_hollonomic:main',
+            'my_robot_wallfollower_holonomic_exec = my_robot_control.my_robot_wallfollower_holonomic:main',
         ],
     },
 )
